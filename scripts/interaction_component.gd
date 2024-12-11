e2c864a48e65e800325552f20ce1e3b10e1a813b
rafeeq-y <rafeeqy@proton.me>
2024-12-11 23:11:32 +0200

    initial framework for grab force

--- scripts/interaction_component.gd
+++ scripts/interaction_component.gd
@@ -16,17 +16,20 @@ func in_range() -> void:
 func out_of_range() -> void:
 	get_mesh().material_overlay = null
 	
-func interacted() -> void:
+func interacted(target) -> void:
+	#TODO: set force direction of object to target
 	print("interacted")
+	print(target)
 	
 func throw() -> void:
+	#use impulse
 	print("throw")
 	
 	
 func connect_parent() -> void:
 	parent.add_user_signal("focused")
 	parent.add_user_signal("unfocused")
-	parent.add_user_signal("interacted")
+	parent.add_user_signal("interacted",[{"name": "target","type": TYPE_VECTOR3}])
 	parent.add_user_signal("throw")
 	parent.connect("focused", Callable(self, "in_range"))
 	parent.connect("unfocused", Callable(self, "out_of_range"))
